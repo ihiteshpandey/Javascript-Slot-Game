14a413fe3af788bd4e6d9e55409c9733b79ef3ac
ihiteshpandey <hiteshpandey183@gmail.com>
2024-04-24 20:35:08 +0530

    File Added

---
+++ Javascript Project/project.js
@@ -0,0 +1,15 @@
+// 1 - Deposit Money 
+// 2 - Determine number of lines to bet on 
+// 3 - Collect Amount for bet
+// 4 - Spin the slot machine 
+// 5 - Check if the user won 
+// 6 - Give the users their winning 
+// 7 - Play again 
+
+// Deposit some money
+
+const prompt = require("prompt-sync")();
+const deposit = ()=>{
+    const depositAmount = prompt("Enter Deposit Amount: ");
+};
+deposit();
